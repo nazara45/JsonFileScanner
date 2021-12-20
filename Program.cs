f30c0436d843ec09b7e5bb87242de3059bf4a7bf
nikoladzavric45@gmail.com <nikoladzavric45@gmail.com>
2021-12-20 17:23:35 +0100

    test

--- Program.cs
+++ Program.cs
@@ -12,38 +12,30 @@ namespace ConsoleAppTest
 {
     class Program
     {
+        static string repositoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDoc‌​uments), "ScanJsonApp");
+        static string folderListPath = Path.Combine(repositoryPath, "FolderList.dat");
+        static string configFilePath = Path.Combine(repositoryPath, "Config.dat");
+        static string allReportsFolderPath = Path.Combine(repositoryPath, "All Reports");
+        static string allReportsFolderListPath = Path.Combine(allReportsFolderPath, "AllReportsFolderList.dat");
+        static string lastReportFolderPath = Path.Combine(repositoryPath, "Last Reports");
+        static string lastReportFolderListPath = Path.Combine(lastReportFolderPath, "LastReportFolderList.dat");
+        static int scanInterval = 3000;
+        static int maxJobs = 5;
+
         static void Main(string[] args)
-        {
-            string docPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDoc‌​uments), "ScanJsonApp");
-            string folderList = Path.Combine(docPath, "FolderList.dat");
-            string configPath = Path.Combine(docPath, "Config.dat");            
+        {                    
             string[] folderPaths = null;
             List<string> processReport;
             List<List<string>> filePaths;
-            bool continousProcess = true;
-            int scanInterval = 3000;
-            int maxJobs = 5;
+            bool continousProcess = true;            
 
-            while(continousProcess)
+            while (continousProcess)
             {
-                try
-                {
-                    string[] readConfig = File.ReadAllLines(configPath);
-                    scanInterval = int.Parse(readConfig[1]) * 1000;
-                    maxJobs = int.Parse(readConfig[2]);
-                }
-                catch (FileNotFoundException ex)
-                {
-                    Console.WriteLine("Config.dat file not found, reverting to default configuration. Error: ({0})", ex.Message);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Config.dat error: " + ex.Message);
-                }               
+                ReadConfig();               
 
                 try
                 {
-                    folderPaths = File.ReadAllLines(folderList);
+                    folderPaths = File.ReadAllLines(folderListPath);
                 }
                 catch (FileNotFoundException ex)
                 {
@@ -62,7 +54,7 @@ namespace ConsoleAppTest
                         filePaths.Add(ScanFolder(path));
                     }
 
-                int n = 0;
+                int counter = 0;
 
                 Console.WriteLine("------------------------------------------");
                 Console.WriteLine("PROCESSING");
@@ -71,28 +63,18 @@ namespace ConsoleAppTest
                 if (filePaths != null)
                     foreach (List<string> files in filePaths)
                     {
+                        Console.WriteLine("Processing folder: {0}\n", folderPaths[counter]);
+
                         processReport = ProcessFiles(files, maxJobs);
                         processReport.Sort();
+                        GenerateReport(processReport, lastReportFolderPath, counter);
+                        GenerateOverallReport(processReport, folderPaths[counter]);
 
-                        if (processReport.Count != 0)
-                        {
-                            Console.WriteLine("Processing folder: {0}\n", folderPaths[n]);
-
-                            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, (n.ToString() + ".txt"))))
-                            {
-                                foreach (string report in processReport)
-                                {
-                                    Console.WriteLine(" - " + report);
-                                    outputFile.WriteLine(report);
-                                }                               
-                            }                            
-
-                            Console.WriteLine("------------------------------------------");
-                        }                      
-
-                        n++;
-                                              
+                        counter++;                                       
                     }
+
+                if(folderPaths != null)
+                    GenerateLastProcessFolderList(folderPaths);
 
                 Console.WriteLine("\nFinished.");
                 Console.WriteLine("\nScanning in {0} seconds...", scanInterval/1000);
@@ -194,6 +176,101 @@ namespace ConsoleAppTest
             
             return fullReport;
 
+        }
+
+        public static void GenerateOverallReport(List<string> processReport, string folderPath)
+        {
+            bool folderAlreadyOnList = false;
+            int counter = 0;
+
+            try
+            {
+                if (!Directory.Exists(allReportsFolderPath))
+                    Directory.CreateDirectory(allReportsFolderPath);
+
+                if (!File.Exists(allReportsFolderListPath))
+                {
+                    using StreamWriter sw = new StreamWriter(allReportsFolderListPath);
+                }
+
+
+                string[] folderListReader = File.ReadAllLines(allReportsFolderListPath);
+
+
+                foreach (string path in folderListReader)
+                {
+                    if (path == folderPath)
+                    {
+                        folderAlreadyOnList = true;
+                        break;
+                    }
+                    else
+                        counter++;
+                }
+
+                if (!folderAlreadyOnList)
+                {
+                    using (StreamWriter addToFolderList = File.AppendText(allReportsFolderListPath))
+                    {
+                        addToFolderList.Write(folderPath + "\n");
+                    }
+
+                    GenerateReport(processReport, allReportsFolderPath, counter);
+                }
+            }
+            catch (IOException) { }
+                       
+        }
+
+        public static void GenerateReport(List<string> processReport, string filePath, int counter)
+        {
+                using (StreamWriter reportFile = new StreamWriter(Path.Combine(filePath, (counter.ToString() + ".txt"))))
+                {
+                    foreach (string report in processReport)
+                    {
+                        Console.WriteLine(" - " + report);
+                        reportFile.WriteLine(report);
+                    }
+
+                    if(processReport.Count == 0) 
+                        reportFile.WriteLine("No .json files found.");
+                }
+
+                Console.WriteLine("------------------------------------------");
+        }
+
+        public static void GenerateLastProcessFolderList(string[] folderPaths)
+        {
+            try
+            {
+                if (!Directory.Exists(lastReportFolderPath))
+                    Directory.CreateDirectory(lastReportFolderPath);
+
+                using (StreamWriter lastProcessFolderList = new StreamWriter(lastReportFolderListPath))
+                {
+                    foreach (string path in folderPaths)
+                        lastProcessFolderList.WriteLine(path);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        public static void ReadConfig()
+        {
+            try
+            {
+                string[] readConfig = File.ReadAllLines(configFilePath);
+                scanInterval = int.Parse(readConfig[1]) * 1000;
+                maxJobs = int.Parse(readConfig[2]);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Config.dat file not found, reverting to default configuration. Error: ({0})", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Config.dat error: " + ex.Message);
+            }
         }
     }
 }
